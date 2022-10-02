9dd63061d7cff39ace9d7a60f5cd1ab38377b27a
RachitShrivastava <rachitshrivastava15@gmail.com>
2022-10-02 00:23:11 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CountingBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 2;
+            var arr = new int[num+1];
+            arr[0] = 0;
+            
+            for(int i = 1; i<= num; i++)
+            {
+                if(i%2 == 0)
+                {
+                    arr[i] = arr[i/2];
+                }
+                else
+                {
+                    arr[i] =  arr[i/2]+1;
+                }
+            }
+            
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
